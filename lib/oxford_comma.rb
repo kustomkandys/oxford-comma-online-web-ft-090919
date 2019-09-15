43424b262ea994321be7f765bfab900cce76f92f
Keri Deemer <kerid18@att.net>
2019-09-15 23:22:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(' and ')
   elsif array.length == 3
-    array.join(' , ')
-  
+    array[-1].prepend(' and ') and array.join
   end
 end
